a3e173234af72a9976aa81d3d7c5316d888b0f95
hope-yao <hope-yao@asu.edu>
2019-02-16 19:16:48 -0700

    training of TOPOLOGY on thermal bi-phase material is  NOT converging

--- thermal/code_tensorflow/thermal_2phase_2D_tf_bc2.py
+++ thermal/code_tensorflow/thermal_2phase_2D_tf_bc2.py
@@ -5,16 +5,29 @@ import tensorflow as tf
 
 
 class Jacobi_block():
-    def __init__(self, num_node, load, mask, rho, resp):
+    def __init__(self, num_node, load, mask, rho, resp, beta):
         # NOTICE: right now for homogeneous anisotropic material only!!
         self.num_node = num_node
         self.load = load
         self.mask = mask
+        self.beta = beta
+        self.nicer_mask = self.apply_topology_filter(mask, self.beta)
         self.rho = rho
         self.rho_1, self.rho_2 = tf.split(self.rho,2)
         self.resp = resp
         self.bc_mask = self.get_bc_mask()
         self.d_matrix = self.get_d_matrix()
+
+    def apply_topology_filter(self, mask, beta):
+        r = np.sqrt(2)
+        d = np.asarray([[np.sqrt(2), 1., np.sqrt(2.)], [1., 0., 1.], [np.sqrt(2), 1., np.sqrt(2)]])
+        w = 1. - d / r
+        x = self.boundary_padding(mask)
+        x = tf.nn.conv2d(x, w.reshape(3,3,1,1), strides=[1,1,1,1], padding='VALID')
+        x /= np.sum(w)
+        # rho
+        rho = ( tf.tanh(beta/2) + tf.tanh(beta*(x-0.5)) ) / (2*tf.tanh(beta/2))
+        return rho
 
     def get_bc_mask(self):
         bc_mask = np.ones((batch_size, num_node, num_node, 1))
@@ -36,7 +49,7 @@ class Jacobi_block():
 
     def get_d_matrix(self):
         paddings = tf.constant([[0, 0], [1, 1], [1, 1], [0, 0]])
-        self.elem_mask = tf.pad(self.mask, paddings, "SYMMETRIC")
+        self.elem_mask = tf.pad(self.nicer_mask, paddings, "SYMMETRIC")
         node_filter = np.asarray([[1 / 4.] * 2] * 2)
         node_filter = np.expand_dims(np.expand_dims(node_filter,-1),-1)
 
@@ -46,10 +59,8 @@ class Jacobi_block():
         d_matrix *= self.bc_mask
         return d_matrix
 
-    def LU_layers(self, input_tensor):
+    def LU_layers(self, input_tensor, mask_tensor):
         from tf_ops_cpp.mask_conv import mask_conv
-        mask_tensor = self.mask
-
         padded_input = self.boundary_padding(input_tensor)  # for boundary consideration
         padded_mask = self.boundary_padding(mask_tensor)  # for boundary consideration
         R_u = mask_conv(padded_input, padded_mask, self.rho)
@@ -57,13 +68,17 @@ class Jacobi_block():
         R_u_bc = tf.pad(R_u_bc[:, :, 1:-1, :], ((0,0), (0, 0),(1, 1), (0, 0)), "constant")  # for boundary consideration
         return R_u_bc
 
+    def forward_pass(self,resp, mask):
+        R_u = self.LU_layers(resp, mask)
+        wx = R_u + self.resp * self.d_matrix
+        return wx
 
     def apply(self, max_itr=10):
         result = {}
         u0 = np.zeros((1, num_node, num_node, 1), 'float32')  # where u is unknown
         result['u_hist'] = [u0]
         for itr in range(max_itr):
-            R_u = self.LU_layers(result['u_hist'][-1])
+            R_u = self.LU_layers(result['u_hist'][-1], self.nicer_mask)
             u = (self.load - R_u) / self.d_matrix  # jacobi formulation of linear system of equation solver
             result['u_hist'] += [u]
 
@@ -73,18 +88,19 @@ class Jacobi_block():
 
     def get_loss(self):
         self.pred_err = tf.reduce_mean(tf.abs(jacobi.prediction - resp_pl))
-        self.penalty = tf.reduce_mean(
-            tf.abs(mask_pl) + tf.abs(1 - mask_pl) + 0.5 - tf.abs(0.5 - mask_pl))  # a W shaped penalty
-        self.mask_err = tf.reduce_mean(tf.abs(mask_pl - mask_data))
-        self.loss = self.pred_err + self.penalty
+        # k = 0.2
+        # self.penalty = tf.reduce_mean(
+        #     tf.abs(k*self.mask) + tf.abs(k*(1 - self.mask)) - 0.5*k - tf.abs(k*(0.5 - self.mask)) )  # a W shaped penalty
+        self.mask_err = tf.reduce_mean(tf.abs(self.nicer_mask - mask_data))
+        self.loss = self.pred_err# + self.penalty
 
     def get_optimizer(self):
 
         if 1:
-            lr = 1
+            lr = 0.1
             # self.optimizer = tf.train.MomentumOptimizer(lr, 0.9)  #
             self.optimizer = tf.train.AdamOptimizer(lr, beta1=0.5)
-            self.grads = self.optimizer.compute_gradients(self.loss, var_list=self.rho)
+            self.grads = self.optimizer.compute_gradients(self.loss, var_list=self.mask)#self.rho)
             self.train_op = self.optimizer.apply_gradients(self.grads)
         else:
             ScipyOptimizerInterface = tf.contrib.opt.ScipyOptimizerInterface
@@ -111,14 +127,25 @@ def load_data_elem_s24():
     mask = data['mask'].reshape(1, num_node-1,num_node-1, 1)
     return num_node, u_img, f_img, mask, rho_1, rho_2
 
+def load_data_elem_s12_more():
+    rho_1, rho_2 =  32., 16.
+    num_node = 13
+    f_img = np.zeros((10,num_node,num_node,1))
+    u_img = np.zeros((10,num_node,num_node,1))
+    mask = np.zeros((10,num_node-1,num_node-1,1))
+    for i in range(1,11,1):
+        data = sio.loadmat('/home/hope-yao/Documents/FEA_Net/thermal/data/bi_phase_data/res12/biphase_12_12_ro16_ro_32_case{}.mat'.format(i))
+        f_img[i-1] = data['f_image'].reshape(1, num_node, num_node, 1)
+        u_img[i-1] = data['u_image'].reshape(1, num_node, num_node, 1)
+        mask[i-1] = data['mask'].reshape(1, num_node - 1, num_node - 1, 1)
 
 def load_data_elem_s12():
     if 1:
-        data = sio.loadmat('/home/hope-yao/Documents/MG_net/data/heat_transfer/Downloads/biphase_12_12_ro16_ro_32.mat')
+        data = sio.loadmat('/home/hope-yao/Documents/FEA_Net/thermal/data/bi_phase_data/res12/biphase_12_12_ro16_ro_32.mat')
         rho_1, rho_2 =  32., 16.
     else:
         data = sio.loadmat('/home/hope-yao/Documents/MG_net/data/heat_transfer/Downloads/biphase_12_12_new.mat')
-    rho_1, rho_2 = 205., 16.
+        rho_1, rho_2 = 205., 16.
     num_node = 13
     f_img = data['f_image'].reshape(1, num_node,num_node, 1)
     u_img = data['u_image'].reshape(1, num_node,num_node, 1)
@@ -170,15 +197,28 @@ if __name__ == "__main__":
     batch_size = 1
     load_pl = tf.placeholder(tf.float32,shape=(batch_size, num_node, num_node, 1))
     resp_pl = tf.placeholder(tf.float32,shape=(batch_size, num_node, num_node, 1)) # defined on the nodes
-    initial_mask =  mask_data #initial_mask = np.ones_like(mask_data)
+    # initial_mask =  mask_data
+    initial_mask = np.ones_like(mask_data)#*0.5
     mask_pl = tf.Variable(initial_value=initial_mask,dtype=tf.float32, name='mask_pl') #defined on the elements
-    rho_pl = tf.Variable([rho_1/2,rho_2*2], tf.float32)#tf.placeholder(tf.float32,shape=(2))
+    rho_pl = tf.Variable([rho_1,rho_2], tf.float32)#tf.placeholder(tf.float32,shape=(2))
+    beta = tf.constant(1.0, tf.float32)#controls the topology mass hyper-parameter
 
     # build network
-    jacobi = Jacobi_block(num_node, load_pl, mask_pl, rho_pl, resp_pl)
-    jacobi.apply(max_itr=300)
-    jacobi.get_loss()
-    jacobi.get_optimizer()
+    jacobi = Jacobi_block(num_node, load_pl, mask_pl, rho_pl, resp_pl, beta)
+    if 0: # forward solving
+        jacobi.apply(max_itr=300)
+        jacobi.get_loss()
+        jacobi.get_optimizer()
+
+    else: #inverse generative
+        jacobi.prediction = jacobi.forward_pass(resp_pl,mask_pl)
+        jacobi.pred_err = tf.reduce_mean(tf.abs(jacobi.prediction - load_pl))
+        # k = 0.2
+        # jacobi.penalty = tf.reduce_mean(
+        #     tf.abs(k*mask_pl) + tf.abs(k*(1 - mask_pl)) - 0.5*k - tf.abs(k*(0.5 - mask_pl)) )  # a W shaped penalty
+        jacobi.mask_err = tf.reduce_mean(tf.abs(mask_pl - mask_data))
+        jacobi.loss = jacobi.pred_err #+ jacobi.penalty
+        jacobi.get_optimizer()
 
     # initialize
     FLAGS = tf.app.flags.FLAGS
@@ -198,15 +238,20 @@ if __name__ == "__main__":
     k2_hist = []
     mask_err_hist = []
     mask_hist = []
-    for itr in tqdm(range(1000)):
-        feed_dict_train = {load_pl: load_data, resp_pl: resp_data}#, mask_pl: mask_data
+    nicer_mask_hist = []
+    beta_val = 0.5
+    for itr in tqdm(range(10000)):
+        if itr % 1000 == 0:
+            beta_val = beta_val * 1.5
+        feed_dict_train = {load_pl: load_data, resp_pl: resp_data, jacobi.beta:beta_val}#, mask_pl: mask_data
         sess.run(jacobi.train_op, feed_dict_train)
         # jacobi.optimizer.minimize(sess,feed_dict_train)
-        loss_value_i, pred_err_i, pred_i, mask_err_i, mask_i, k1_value_i, k2_value_i = sess.run([jacobi.loss,
+        loss_value_i, pred_err_i, pred_i, mask_err_i, mask_i, nicer_mask_i, k1_value_i, k2_value_i = sess.run([jacobi.loss,
                                                                                                     jacobi.pred_err,
                                                                                                     jacobi.prediction,
                                                                                                     jacobi.mask_err,
                                                                                                     jacobi.mask,
+                                                                                                    jacobi.nicer_mask,
                                                                                                     jacobi.rho_1,
                                                                                                     jacobi.rho_2],
                                                                                                    feed_dict_train)
@@ -218,11 +263,21 @@ if __name__ == "__main__":
         pred_hist += [pred_i]
         mask_err_hist += [mask_err_i]
         mask_hist += [mask_i]
+        nicer_mask_hist += [nicer_mask_i]
 
-        # plt.imshow(sess.run(jacobi.prediction, feed_dict_train)[0, :, :, 0], cmap='jet', interpolation='bilinear')
-    plt.subplot(1, 2, 1)
-    plt.imshow(np.squeeze(resp_data))
-    plt.subplot(1, 2, 2)
-    plt.imshow(np.squeeze(pred_hist[0]))
+    plt.subplot(1, 5, 1)
+    plt.imshow(np.squeeze(resp_data[0]))
+    plt.colorbar()
+    plt.subplot(1, 5, 2)
+    plt.imshow(np.squeeze(pred_hist[-1][0]))
+    plt.colorbar()
+    plt.subplot(1, 5, 3)
+    plt.imshow(np.squeeze(mask_data[0]))
+    plt.colorbar()
+    plt.subplot(1, 5, 4)
+    plt.imshow(np.squeeze(mask_hist[-1][0]))
+    plt.colorbar()
+    plt.subplot(1, 5, 5)
+    plt.imshow(np.squeeze(nicer_mask_hist[-1][0]))
     plt.colorbar()
     print('done')
